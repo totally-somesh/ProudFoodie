4265ad4269ff01b69fed94f9d79350f386081529
totally-somesh <somesh.dahotre1@gmail.com>
2023-10-11 23:55:38 +0530

    Resolved System.IO.InvalidDataException

---
+++ ProudFoodie/Models/EmailSender.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace ProudFoodie.Models
+{
+    public class EmailSender : IEmailSender
+    {
+        Task IEmailSender.SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
